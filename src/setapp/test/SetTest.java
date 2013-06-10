7fee269701b1c53861e30bdf66a1c5c8df1d55b9
HannahRose <hannah.rose@spacelabs.com>
2013-06-10 13:48:03 -0700

    created testNG class

---
+++ src/setapp/test/SetTest.java
@@ -0,0 +1,18 @@
+package setapp.test;
+
+import org.testng.annotations.*;
+
+public class SetTest {
+  @Test
+  public void f() {
+  }
+  
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
